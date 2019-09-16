6632ab43a21792b98e1b5f083f174b681a2a448b
Jim West <jim@west.net.nz>
2019-09-16 14:53:39 +1200

    Initial load

---
+++ swim/rwc19/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+
+# Register your models here.
+# Register your models here.
+from .models import Profile, Team, Game, Prediction
+
+admin.site.register(Profile)
+admin.site.register(Team)
+admin.site.register(Game)
+admin.site.register(Prediction)
---
+++ swim/rwc19/models.py
@@ -0,0 +1,50 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+import datetime
+from django.utils import timezone
+
+# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phoneNumber = models.CharField(max_length=50, blank=True, null=True)
+    
+    def __str__(self):
+        return self.user.username
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+	if created:
+		Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+	instance.profile.save()
+
+class Team(models.Model):
+    teamID = models.CharField(max_length=30)
+    descr = models.TextField(blank=True, null=True)
+    
+    def __str__(self):
+        return self.teamID
+
+class Game(models.Model):
+    Team1 = models.ForeignKey(Team, on_delete=models.CASCADE)
+    Team2 = models.ForeignKey(Team, on_delete=models.CASCADE, related_name="team2",)
+    gamedate = models.DateTimeField(blank=True, null=True)
+    score1 = models.IntegerField(default=0, help_text="Score of 1st teame")
+    score2 = models.IntegerField(default=0, help_text="Score of 2nd teame")
+
+    def __str__(self):
+        return("{} v {}".format(self.Team1, self.Team2))
+
+class Prediction(models.Model):
+    player = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    score1 = models.IntegerField(default=0, help_text="Score of 1st teame")
+    score2 = models.IntegerField(default=0, help_text="Score of 2nd teame")
+
+    def __str__(self):
+        return("Player : {}, Game : {}".format(self.player, self.game))
