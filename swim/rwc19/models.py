28de002a6f0af98b1e6e13f864f22f40fdcf52d8
Jim West <jim@west.net.nz>
2019-10-15 20:53:34 +1300

    Setting up testing

--- swim/rwc19/models.py
+++ swim/rwc19/models.py
@@ -4,6 +4,7 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 import datetime
 from django.utils import timezone
+from django.core.validators import MinValueValidator
 
 # Create your models here.
 class Profile(models.Model):
@@ -48,8 +49,8 @@ class Game(models.Model):
     Team1 = models.ForeignKey(Team, on_delete=models.CASCADE)
     Team2 = models.ForeignKey(Team, on_delete=models.CASCADE, related_name="team2",)
     gamedate = models.DateTimeField(blank=True, null=True)
-    score1 = models.IntegerField(default=0, help_text="Score of 1st team")
-    score2 = models.IntegerField(default=0, help_text="Score of 2nd team")
+    score1 = models.IntegerField(default=0, help_text="Score of 1st team", validators=[MinValueValidator(0,'Negative scores not allowed!')])
+    score2 = models.IntegerField(default=0, help_text="Score of 2nd team", validators=[MinValueValidator(0,'Negative scores not allowed!')])
     started = models.BooleanField(default=False)
     finished = models.BooleanField(default=False)
     high_point = models.FloatField(default=0, )
@@ -64,8 +65,8 @@ class Game(models.Model):
 class Prediction(models.Model):
     player = models.ForeignKey(Profile, on_delete=models.CASCADE)
     game = models.ForeignKey(Game, on_delete=models.CASCADE)
-    score1 = models.IntegerField(default=0, help_text="Score of 1st team")
-    score2 = models.IntegerField(default=0, help_text="Score of 2nd team")
+    score1 = models.IntegerField(default=0, help_text="Score of 1st team", validators=[MinValueValidator(0,'Negative scores not allowed!')])
+    score2 = models.IntegerField(default=0, help_text="Score of 2nd team", validators=[MinValueValidator(0,'Negative scores not allowed!')])
     result = models.BooleanField(default=False)
     points = models.FloatField(default=0, help_text="For and away points")
     textname = models.CharField(max_length=50, blank=True, null=True)
--- swim/rwc19/tests.py
+++ swim/rwc19/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from rwc19.models import Profile
+from django.contrib.auth.models import User
 
 # Create your tests here.
+
+def create_profiles():
+    user_jim = User.objects.create(username = 'jim', email = 'jim@west.net.nz', password = 'jimpassword')
+    user_jim.save()
+    user_john = User.objects.create(username = 'john', email = 'john@west.net.nz', password = 'johnpassword')
+    user_john.save()
+    user_jane = User.objects.create(username = 'jane', email = 'jane@west.net.nz', password = 'janepassword')
+    user_jane.save()
+
+class ProfileModelTests(TestCase):
+    def test_positive_total_points(self):
+        create_profiles()
+        user_jim = User.objects.get(username='jim')
+        profile_jim = Profile(user=user_jim)
+        self.assertIs(profile_jim.totalPoints >= 0, True )
