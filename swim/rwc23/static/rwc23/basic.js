5c8e08f6158d7f770f33fe3a766ea14873115f1a
Jim West <jim@west.net.nz>
2023-09-06 17:53:58 +1200

    typo fix

--- swim/rwc23/static/rwc23/basic.js
+++ swim/rwc23/static/rwc23/basic.js
@@ -6,3 +6,16 @@ function submitform(colID)
   document.selectForm.submit();
 }
 
+function hideItem(id)
+{
+  const element = document.getElementById(id)
+  element.classList.remove("showItem"); // Remove showItem
+  element.classList.add("hideItem"); // Add showItem
+}
+
+function showItem(id)
+{
+  const element = document.getElementById(id)
+  element.classList.remove("hideItem"); // Remove hideItem
+  element.classList.add("showItem"); // Add showItem
+}
